eb50d35df6a17ee25b1cd1317919c4af0d9516ff
Meshari466 <86444163+Meshari466@users.noreply.github.com>
2021-07-01 05:34:31 +0300

    Add files via upload

---
+++ calculate_revenue.py
@@ -0,0 +1,8 @@
+x=float(input("enter total sales:"))
+y=float(input("enter 2st number :"))
+z=float(input("enter 3st number :"))
+
+print("annual_gross_revenue is : ", max(x, y ,z))
+print("monthly_gross_revenue is : ", max(x, y ,z))
+print("quarterly_gross_revenue is : ",max(x , y ,z))
+input("press any key to exit")
